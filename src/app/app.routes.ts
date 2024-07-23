1fcb1c257f492d8762f67fcae159352d21a94c2d
D240021 <danielbarrantesq@gmail.com>
2024-07-23 14:45:47 -0600

    Responsive styles for page Home  and page About added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { provideRouter, withHashLocation, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home/home.component';
 import { ProjectsComponent } from './pages/projects/projects/projects.component';
+import { AboutComponent } from './pages/about/about/about.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'projects', component: ProjectsComponent }
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'about', component: AboutComponent }
 ];
 
 
